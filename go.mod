5e103480c40b3d0a76353cfe8860ce02e858649e
Jerod <jerod@synth.sh>
2024-04-24 08:05:14 -0700

    test python script

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xjerod/transcribe
+
+go 1.22.2
+
+require github.com/alphacep/vosk-api/go v0.3.50
---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="mx-auto flex h-full min-h-0 max-w-screen-md flex-col justify-center p-2">
+	<div class="flex flex-grow flex-col justify-end overflow-y-scroll p-2 px-4">
+		<div class="w-full py-1 text-gray-800">message 1</div>
+		<div class="w-full py-1 text-gray-800">message 2</div>
+	</div>
+	<div class="divider px-4"></div>
+	<div class="flex p-2 px-4 pb-4 text-gray-400">This is an incoming subtitle</div>
+</div>
