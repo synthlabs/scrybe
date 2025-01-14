36bc562ffb0d799519bce612e22232e492f1934e
Jerod <jerod@synth.sh>
2025-01-14 10:03:35 -0800

    add transparency slider and update overlay settings layout

--- src/routes/(app)/overlay/+page.svelte
+++ src/routes/(app)/overlay/+page.svelte
@@ -5,6 +5,7 @@
     import TextOverlay from "$lib/components/overlay/text-overlay.svelte";
     import { Separator } from "$lib/components/ui/separator/index.ts";
     import { Label } from "$lib/components/ui/label/index.ts";
+    import { Slider } from "$lib/components/ui/slider/index.ts";
     import { Input } from "$lib/components/ui/input/index.ts";
     import { cn } from "$lib/utils";
     import { SyncedStore } from "$lib/store.svelte";
@@ -18,9 +19,11 @@
     let store = new SyncedStore<AppState>("appstate", DefaultAppState);
     store.init();
 
-    let color = $state("#030712");
+    let bg_color = $state("#030712");
+    let transparency = $state([75]);
 
-    $inspect(color);
+    $inspect(bg_color);
+    $inspect(transparency);
 
     // onMount(async () => {
     //     // const store = await load("overlay.json", { autoSave: true });
@@ -48,70 +51,97 @@
         <div class="bg-checkered h-32 w-full border-2 border-primary">
             <TextOverlay
                 justify={store.object.overlay_config.text_alignment}
-                background={color}
+                background={bg_color}
+                transparency={transparency[0]}
             ></TextOverlay>
         </div>
-        <div class="flex flex-col gap-2 pt-4">
-            <Label
-                id="align-label"
-                for="align"
-                class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-                Align
-            </Label>
-            <div id="align" class="row flex flex-row">
-                <button
-                    class={cn(
-                        "rounded-md rounded-r-none border border-transparent  px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-accent hover:shadow-lg focus:bg-slate-600 focus:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
-                        store.object.overlay_config.text_alignment == "left"
-                            ? "bg-accent"
-                            : "bg-secondary",
-                    )}
-                    onclick={() =>
-                        (store.object.overlay_config.text_alignment = "left")}
-                    type="button"
+        <div class="flex w-full flex-row flex-wrap gap-4">
+            <div class="flex flex-col gap-2 pt-4">
+                <Label
+                    id="align-label"
+                    for="align"
+                    class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                 >
-                    <AlignLeft />
-                </button>
-                <button
-                    class={cn(
-                        "rounded-none border border-transparent  px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-accent hover:shadow-lg focus:bg-slate-600 focus:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
-                        store.object.overlay_config.text_alignment == "center"
-                            ? "bg-accent"
-                            : "bg-secondary",
-                    )}
-                    onclick={() =>
-                        (store.object.overlay_config.text_alignment = "center")}
-                    type="button"
-                >
-                    <AlignCenter />
-                </button>
-                <button
-                    class={cn(
-                        "rounded-md rounded-l-none border border-transparent  px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-accent hover:shadow-lg focus:bg-slate-600 focus:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
-                        store.object.overlay_config.text_alignment == "right"
-                            ? "bg-accent"
-                            : "bg-secondary",
-                    )}
-                    onclick={() =>
-                        (store.object.overlay_config.text_alignment = "right")}
-                    type="button"
-                >
-                    <AlignRight />
-                </button>
+                    Align
+                </Label>
+                <div id="align" class="row flex flex-row">
+                    <button
+                        class={cn(
+                            "rounded-md rounded-r-none border border-transparent  px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-accent hover:shadow-lg focus:bg-slate-600 focus:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+                            store.object.overlay_config.text_alignment == "left"
+                                ? "bg-accent"
+                                : "bg-secondary",
+                        )}
+                        onclick={() =>
+                            (store.object.overlay_config.text_alignment =
+                                "left")}
+                        type="button"
+                    >
+                        <AlignLeft />
+                    </button>
+                    <button
+                        class={cn(
+                            "rounded-none border border-transparent  px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-accent hover:shadow-lg focus:bg-slate-600 focus:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+                            store.object.overlay_config.text_alignment ==
+                                "center"
+                                ? "bg-accent"
+                                : "bg-secondary",
+                        )}
+                        onclick={() =>
+                            (store.object.overlay_config.text_alignment =
+                                "center")}
+                        type="button"
+                    >
+                        <AlignCenter />
+                    </button>
+                    <button
+                        class={cn(
+                            "rounded-md rounded-l-none border border-transparent  px-4 py-2 text-center text-sm text-white shadow-md transition-all hover:bg-accent hover:shadow-lg focus:bg-slate-600 focus:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none",
+                            store.object.overlay_config.text_alignment ==
+                                "right"
+                                ? "bg-accent"
+                                : "bg-secondary",
+                        )}
+                        onclick={() =>
+                            (store.object.overlay_config.text_alignment =
+                                "right")}
+                        type="button"
+                    >
+                        <AlignRight />
+                    </button>
+                </div>
             </div>
+            <div class="flex flex-col gap-2 pt-4">
+                <Label
+                    id="color-label"
+                    for="bg-color"
+                    class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                    Background Color
+                </Label>
+                <div id="bg-color" class="flex flex-grow flex-row items-center">
+                    <input type="color" bind:value={bg_color} />
+                    <Slider
+                        bind:value={transparency}
+                        max={100}
+                        step={1}
+                        class="w-72 px-2"
+                    />
+                </div>
+            </div>
+            <div class="flex flex-col gap-2 pt-4">another setting</div>
+            <div class="flex flex-col gap-2 pt-4">another setting</div>
         </div>
-        <div class=""><input type="color" bind:value={color} /></div>
     </div>
 </div>
 
 <style>
     .bg-checkered {
         background-image: linear-gradient(
-                rgba(0, 0, 0, 0.25),
-                rgba(0, 0, 0, 0.25)
+                rgba(110, 110, 110, 0.6),
+                rgba(110, 110, 110, 0.6)
             ),
-            url("data:image/svg+xml,%0A%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='%23F0F0F0' d='M0 0h8v8H0zm8 8h8v8H8z'/%3E%3C/svg%3E");
-        background-size: 16px 16px;
+            url("data:image/svg+xml,%0A%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='%23F0F0F0' d='M0 0h16v16H0zm16 16h16v16H16z'/%3E%3C/svg%3E");
+        background-size: 32px 32px;
     }
 </style>
