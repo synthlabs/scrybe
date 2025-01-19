f96e5f1df659c624062af4cae350b1ec990959c7
Jerod <jerod@synth.sh>
2025-01-19 12:47:20 -0800

    websocket segment updates on the overlay

--- src/routes/v1/overlay/+page.svelte
+++ src/routes/v1/overlay/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
+    import type { WhisperSegment } from "$bindings/WhisperSegment";
     import TextOverlay from "$lib/components/overlay/text-overlay.svelte";
+
+    let current_segment: WhisperSegment = $state({
+        id: "",
+        index: 0,
+        items: [],
+    });
 
     const ws = new WebSocket("ws://localhost:3030/ws");
     ws.onmessage = (event) => {
-        console.log(event);
+        let segment: WhisperSegment = JSON.parse(event.data);
+        current_segment = segment;
     };
     ws.onopen = (event) => {
         console.log("connected", event);
@@ -11,4 +19,13 @@
     };
 </script>
 
-<TextOverlay />
+<div class="h-full w-full">
+    <TextOverlay {current_segment} />
+</div>
+
+<style>
+    :global(body) {
+        height: 100%;
+        width: 100%;
+    }
+</style>
